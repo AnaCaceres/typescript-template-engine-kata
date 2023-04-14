a750deb70b83c5afe1802eb2042055c8874bcd98
Ana Cáceres <ana.caceres@leanmind.es>
2023-04-14 12:54:17 +0200

    GREEN: add fake implementation for templateEngine function

---
+++ src/TemplateEngine.ts
@@ -0,0 +1,5 @@
+function templateEngine(template: string, values: {}) {
+  return 'This template with one foo';
+}
+
+export { templateEngine };
--- tests/TemplateEngine.spec.ts
+++ tests/TemplateEngine.spec.ts
@@ -1,8 +1,10 @@
+import { templateEngine } from "../src/TemplateEngine";
+
 describe("Template Engine", () => {
   it("should replace variable with corresponding value", () => {
-    const template = `This is a template with one ${variable}`;
+    const template = 'This is a template with one ${variable}';
     const values = { variable: "foo" };
 
-    expect(templateEngine(template, value)).toBe('This template with one foo');
+    expect(templateEngine(template, values)).toBe('This template with one foo');
   });
 });
