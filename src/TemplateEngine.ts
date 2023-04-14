15bf74605d01cabe2d18d0dab0f66fafcdc96d2e
Ana Cáceres <ana.caceres@leanmind.es>
2023-04-14 14:57:10 +0200

    GREEN: Add warning when there are no values

--- src/TemplateEngine.ts
+++ src/TemplateEngine.ts
@@ -1,8 +1,14 @@
-function templateEngine(template: string, values: {[key: string]: string}) {
+function templateEngine(template: string, values: {[key: string]: string | number}) {
+  if (Object.keys(values).length === 0) {
+    console.warn('No values to replace in template');
+    return template;
+  }
+
   let transformTemplate = template;
 
   for (const valueKey in values) {
-    transformTemplate = transformTemplate.replaceAll('${'+ `${valueKey}` + '}', values[valueKey]);
+    const value = values[valueKey].toString();
+    transformTemplate = transformTemplate.replaceAll('${'+ `${valueKey}` + '}', value);
   }
 
   return transformTemplate;
--- tests/TemplateEngine.spec.ts
+++ tests/TemplateEngine.spec.ts
@@ -24,4 +24,28 @@ describe("Template Engine", () => {
     "And this is another text with other-value to be replaced. " +
     "And this is another text with another-value to be replaced.");
   });
+  it("should replace variables with all corresponding values with different types", () => {
+    const template = "This is a text with a ${variable} to be replaced. " +
+    "And this is another text with ${other-variable} to be replaced. " +
+    "And this is another text with ${another-variable} to be replaced.";
+
+    const values = { variable: "", "other-variable": 1, "another-variable": "another-value" };
+
+    expect(templateEngine(template, values)).toBe("This is a text with a  to be replaced. " +
+    "And this is another text with 1 to be replaced. " +
+    "And this is another text with another-value to be replaced.");
+  });
+  it("should return the template if there are no values to replace", () => {
+    const template = "This is a text with a ${variable} to be replaced. " +
+    "And this is another text with ${other-variable} to be replaced. " +
+    "And this is another text with ${another-variable} to be replaced.";
+
+    const values = {};
+    const spy = jest.spyOn(console, 'warn');
+
+    expect(templateEngine(template, values)).toBe("This is a text with a ${variable} to be replaced. " +
+    "And this is another text with ${other-variable} to be replaced. " +
+    "And this is another text with ${another-variable} to be replaced.");
+    expect(spy).toHaveBeenCalled();
+  });
 });
