eb82a64f61ebbb023cadbb8f31643eb88f583f82
Ana Cáceres <ana.caceres@leanmind.es>
2023-04-14 13:27:38 +0200

    RED: add templateEngine test with multiple variables

--- tests/TemplateEngine.spec.ts
+++ tests/TemplateEngine.spec.ts
@@ -13,4 +13,15 @@ describe("Template Engine", () => {
 
     expect(templateEngine(template, values)).toBe('This is a template with other foo');
   });
+  it("should replace variables with all corresponding values", () => {
+    const template = "This is a text with a ${variable} to be replaced. \n" +
+    "And this is another text with ${other-variable} to be replaced. \n" +
+    "And this is another text with ${another-variable} to be replaced.";
+
+    const values = { variable: "value", "other-variable": "other-value", "another-variable": "another-value" };
+
+    expect(templateEngine(template, values)).toBe("This is a text with a variable to be replaced. " +
+    "And this is another text with other-value to be replaced. " +
+    "And this is another text with another-value to be replaced.");
+  });
 });
